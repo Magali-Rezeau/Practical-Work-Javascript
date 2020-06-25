873bf428b8aff2fccc51d2131833fd6d7f9e912e
Magali Rézeau <magalirezeau@gmail.com>
2020-06-25 17:45:43 +0200

    edit a todo

--- TodoList/src/index.js
+++ TodoList/src/index.js
@@ -7,11 +7,13 @@ const input = document.querySelector('input');
 const todos = [
   {
     text: "Je suis une todo",
-    done: true
+    done: false,
+    editMode: true
   },
   {
     text: "Je suis une autre todo",
-    done: false
+    done: true,
+    editMode: false
   }
 ];
 
@@ -24,7 +26,13 @@ form.addEventListener('submit', (event) => {
 
 const displayTodo = () => {
   const todoNode = todos.map((todo, index) => {
-    return createTodoElement(todo, index);
+    console.log(todo.editMode);
+    if(todo.editMode) {
+      return createTodoEditElement(todo, index);
+    }else {
+      return createTodoElement(todo, index);
+      console.log(todo);
+    }
   });
   ul.innerHTML = "";
   ul.append(...todoNode);
@@ -33,28 +41,58 @@ const displayTodo = () => {
 const createTodoElement = (todo, index) => {
   const li = document.createElement('li');
   const deleteButton = document.createElement('button');
+  const editButton = document.createElement('button');
   deleteButton.innerHTML = "Supprimer";
+  editButton.innerHTML = "Editer";
 
   deleteButton.addEventListener("click", event => {
     event.stopPropagation();
     deleteTodo(index);
+  });
+  editButton.addEventListener("click", event => {
+    event.stopPropagation();
+    toggleEditMode(index);
   });
 
   li.innerHTML = `
     <span class="todo${ todo.done ? " done" : ""}"></span>
     <p>${ todo.text }</p>
   `;
-  li.appendChild(deleteButton);
+  li.append(deleteButton, editButton);
 
   li.addEventListener("click", event => {
     toggleTodo(index);
   });
-  
+
   return li;
 };
 
+const createTodoEditElement = (todo, index) => {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  input.type = "text";
+  input.value = todo.text;
+  const saveButton = document.createElement('button');
+  saveButton.innerHTML = "Save";
+  const cancelButton = document.createElement('button');
+  cancelButton.innerHTML = "Cancel";
 
-const addTodo = (text) => {
+  saveButton.addEventListener("click", event => {
+    event.stopPropagation();
+    editTodo(index, input);
+  });
+
+  cancelButton.addEventListener("click", event => {
+    event.stopPropagation();
+    toggleEditMode(index);
+  });
+
+  li.append(input, saveButton, cancelButton);
+
+  return li;
+};
+
+const addTodo = text => {
   todos.push(
     {
       text,
@@ -64,14 +102,26 @@ const addTodo = (text) => {
   displayTodo();
 };
 
-const deleteTodo = (index) => {
+const deleteTodo = index => {
   todos.splice(index, 1);
   displayTodo();
 };
 
-const toggleTodo = (index) => {
+const toggleTodo = index => {
   todos[index].done = !todos[index].done;
   displayTodo();
 };
+
+const editTodo = (index, input) => {
+  const value = input.value;
+  todos[index].text = value;
+  todos[index].editMode = !todos[index].editMode;
+  displayTodo();
+};
+
+const toggleEditMode = index => {
+  todos[index].editMode = !todos[index].editMode;
+  displayTodo();
+}
 
 displayTodo();
